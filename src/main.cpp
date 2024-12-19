fce61f5bc9b978cac29f3c6e66a125055f1068c7
Haaziq386 <haaziqjamal@gmail.com>
2024-12-19 15:31:35 +0400

    added some code for checking cpu and memory utilization

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,88 @@
 #include "order_manager.hpp"
 #include "utils.hpp"
 #include "websocket_con.hpp"
+#include "sys/times.h"
+
+int parseLine(char *line)
+{
+    // This assumes that a digit will be found and the line ends in " Kb".
+    int i = strlen(line);
+    const char *p = line;
+    while (*p < '0' || *p > '9')
+        p++;
+    line[i - 3] = '\0';
+    i = atoi(p);
+    return i;
+}
+
+int getValue()
+{ // Note: this value is in KB!
+    FILE *file = fopen("/proc/self/status", "r");
+    int result = -1;
+    char line[128];
+
+    while (fgets(line, 128, file) != NULL)
+    {
+        if (strncmp(line, "VmRSS:", 6) == 0)
+        {
+            result = parseLine(line);
+            break;
+        }
+    }
+    fclose(file);
+    return result;
+}
+
+static clock_t lastCPU, lastSysCPU, lastUserCPU;
+static int numProcessors;
+
+void init()
+{
+    FILE *file;
+    struct tms timeSample;
+    char line[128];
+
+    lastCPU = times(&timeSample);
+    lastSysCPU = timeSample.tms_stime;
+    lastUserCPU = timeSample.tms_utime;
+
+    file = fopen("/proc/cpuinfo", "r");
+    numProcessors = 0;
+    while (fgets(line, 128, file) != NULL)
+    {
+        if (strncmp(line, "processor", 9) == 0)
+            numProcessors++;
+    }
+    fclose(file);
+}
+
+double getCurrentValue()
+{
+    struct tms timeSample;
+    clock_t now;
+    double percent;
+
+    now = times(&timeSample);
+    if (now <= lastCPU || timeSample.tms_stime < lastSysCPU ||
+        timeSample.tms_utime < lastUserCPU)
+    {
+        // Overflow detection. Just skip this value.
+        percent = -1.0;
+    }
+    else
+    {
+        percent = (timeSample.tms_stime - lastSysCPU) +
+                  (timeSample.tms_utime - lastUserCPU);
+        percent /= (now - lastCPU);
+        percent /= numProcessors;
+        percent *= 100;
+    }
+    lastCPU = now;
+    lastSysCPU = timeSample.tms_stime;
+    lastUserCPU = timeSample.tms_utime;
+
+    return percent;
+}
 
 // Define actions
 enum Action
@@ -216,6 +298,8 @@ void orderManagementSystem(OrderManager &orderManager)
         case EXIT:
         {
             std::cout << "Exiting order management system...\n";
+            std::cout << "Memory usage" << getValue() << "KB\n";
+            std::cout << "CPU usage: " << getCurrentValue() << "%\n";
             return;
         }
         default:
@@ -227,6 +311,8 @@ void orderManagementSystem(OrderManager &orderManager)
 
 int main()
 {
+    init(); // Initialize CPU usage tracking
+
     // examples
     //"USDC_USDT", "buy", 10.0, 350.0,"market"  ->spot
     //"BTC-PERPETUAL", "buy", 10.0, 350.0,"limit" ->future
